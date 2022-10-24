fb307b4a36456cdded0c80ee132e94fe953160e4
santiago hernandez <santylopes62@gmail.com>
2022-10-24 12:13:17 -0500

    html y ccs

---
+++ js/index.js
@@ -0,0 +1,37 @@
+//   declarar variables para vincular los objetos del formulario
+const usuario = document.getElementById('user');
+const password = document.getElementById('pass');
+const formulario = document.getElementById('formlogin');
+
+//Generando o leyendo en¿ventos 
+formulario.addEventListener('submit', login);
+
+// Funciones a ejecutar
+function login(e){
+    e.preventDefault();
+    
+    let usuarioVal = usuario.value;
+    let passwordVal = password.value;
+
+    if(usuarioVal == '' || passwordVal == ''){
+        creaMensaje('Verifica tus campos','danger');
+        return;
+    }
+
+    if(localStorage.getItem('usuario')){
+        let objeto = JSON.parse (localStorage.getItem('usuario'));
+
+        if(usuarioVal == objeto.user && passwordVal == objeto.pass) {
+            creaMensaje('Usuario Correcto','success');
+            localStorage.setItem('sesion','activa');
+            setTimeout(function(){
+                window.open('./inicio.html','_self');
+            },2000);
+        }else{
+            creaMensaje('Usuario Incorrecto','danger');
+        }
+    }else{
+        creaMensaje('No hay registros','danger');
+    }
+
+}
---
+++ js/registro.js
@@ -0,0 +1,38 @@
+//declaracion variables de referencia a objetos
+const nombre = document.getElementById('name');
+const email = document.getElementById('email');
+const user = document.getElementById('user');
+const pass = document.getElementById('pass');
+const formulario = document.getElementById('formRegistro');
+
+formulario.addEventListener('submit', registro);
+
+function registro(e){
+    e.preventDefault();
+    
+    let nombreVal = nombre.value;
+    let emailVal = email.vaue;
+    let userVal = user.value;
+    let passVal = pass.value;
+    
+    if(nombreVal == '' || emailVal == '' || userVal == '' || passVal == ''){
+        creaMensaje('Verifica tus campos','danger');
+        return;
+    }
+
+    const usuario = {
+        nombre: nombreVal,
+        email: emailVal,
+        user: userVal,
+        pass: passVal,
+    }
+
+    localStorage.setItem('usuario', JSON.stringify(usuario));
+
+     nombre.value = '';
+     email.vaue = '';
+     user.value = '';
+     pass.value = '';
+
+    creaMensaje('Usuario Registrado!','seccess');
+}
